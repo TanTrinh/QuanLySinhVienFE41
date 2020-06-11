495c1ee3d28e1f1830a7d83fe18fa668987323f3
TanTrinh <trinhhoangtan96@gmail.com>
2020-06-11 19:22:26 +0700

    merge code from v3 of user B

--- models/DanhSachSinhVien.js
+++ models/DanhSachSinhVien.js
@@ -2,15 +2,11 @@ var DanhSachSinhVien = function() {
     this.dssv = [];
     this.themSinhVien = function(sv) {
         this.dssv.push(sv);
-<<<<<<< HEAD
     }
     this.hienThiThongTinSinhVien = function(){
         console.log("Hien thi Thong Tin SV");
     }
-=======
-    };
     this.capNhatSinhVien = function(maSV, sinhVien) {
         console.log('chức năng cập nhật sinh viên!');
-    };
->>>>>>> 3396f2c4d8ef1ba37bb3e1d48e6728f637447cd2
+    }
 }
