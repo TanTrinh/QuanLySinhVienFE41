911b1b8a08cc662bafc6408e33f78086a03553cf
unknown <dotrunghoai@gmail.com>
2020-06-11 19:45:20 +0700

    Ver 4

--- models/SinhVien.js
+++ models/SinhVien.js
@@ -6,4 +6,8 @@ var SinhVien = function (){
     this.DiemToan = '';
     this.DiemLy = '';
     this.DiemHoa = '';
+    this.xuatThongTinSinhVien = function() {
+        console.log(this.TenSV);
+        console.log(this.Email);
+    }
 }
