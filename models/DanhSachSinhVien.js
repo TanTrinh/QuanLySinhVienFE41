3396f2c4d8ef1ba37bb3e1d48e6728f637447cd2
TanTrinh <trinhhoangtan96@gmail.com>
2020-06-11 18:58:13 +0700

    xay dung chuc nang cap nhat sinh vien

--- models/DanhSachSinhVien.js
+++ models/DanhSachSinhVien.js
@@ -2,5 +2,8 @@ var DanhSachSinhVien = function() {
     this.dssv = [];
     this.themSinhVien = function(sv) {
         this.dssv.push(sv);
-    }
+    };
+    this.capNhatSinhVien = function(maSV, sinhVien) {
+        console.log('chức năng cập nhật sinh viên!');
+    };
 }
