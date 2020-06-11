f57686d26bdf44c30e9bf8e7ebd08d5cbc19c3fa
unknown <dotrunghoai@gmail.com>
2020-06-11 19:14:42 +0700

    update lan 2

--- models/DanhSachSinhVien.js
+++ models/DanhSachSinhVien.js
@@ -3,4 +3,7 @@ var DanhSachSinhVien = function() {
     this.themSinhVien = function(sv) {
         this.dssv.push(sv);
     }
+    this.hienThiThongTinSinhVien = function(){
+        console.log("Hien thi Thong Tin SV");
+    }
 }
